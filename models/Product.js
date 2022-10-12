c9ee201e263caa9756eb0ecf19772b4db5bedd6a
Jesse Pence <jessepence@gmail.com>
2022-10-12 14:11:20 -0700

    models

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
   {
@@ -7,11 +7,10 @@ const productSchema = new mongoose.Schema(
     category: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
-    brand: { type: String, required: true },
-    rating: { type: Number, required: true, default: 0 },
-    numReviews: { type: Number, required: true, default: 0 },
     countInStock: { type: Number, required: true, default: 0 },
     description: { type: String, required: true },
+    tags: { type: String, required: true },
+    isRented: { type: Boolean, default: false },
   },
   {
     timestamps: true,
@@ -19,5 +18,5 @@ const productSchema = new mongoose.Schema(
 );
 
 const Product =
-  mongoose.models.Product || mongoose.model('Product', productSchema);
+  mongoose.models.Product || mongoose.model("Product", productSchema);
 export default Product;
