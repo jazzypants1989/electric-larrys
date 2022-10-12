c19e4a6f9bb2b7e6ba7cb814484daf798f6ebb24
Jesse Pence <jessepence@gmail.com>
2022-10-12 14:43:30 -0700

    little by little

--- pages/api/admin/products/[id]/index.js
+++ pages/api/admin/products/[id]/index.js
@@ -1,22 +1,22 @@
-import { getSession } from 'next-auth/react';
-import Product from '../../../../../models/Product';
-import db from '../../../../../utils/db';
+import { getSession } from "next-auth/react";
+import Product from "../../../../../models/Product";
+import db from "../../../../../utils/db";
 
 const handler = async (req, res) => {
   const session = await getSession({ req });
   if (!session || (session && !session.user.isAdmin)) {
-    return res.status(401).send('signin required');
+    return res.status(401).send("signin required");
   }
 
   const { user } = session;
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     return getHandler(req, res, user);
-  } else if (req.method === 'PUT') {
+  } else if (req.method === "PUT") {
     return putHandler(req, res, user);
-  } else if (req.method === 'DELETE') {
+  } else if (req.method === "DELETE") {
     return deleteHandler(req, res, user);
   } else {
-    return res.status(400).send({ message: 'Method not allowed' });
+    return res.status(400).send({ message: "Method not allowed" });
   }
 };
 const getHandler = async (req, res) => {
@@ -34,15 +34,15 @@ const putHandler = async (req, res) => {
     product.price = req.body.price;
     product.category = req.body.category;
     product.image = req.body.image;
-    product.brand = req.body.brand;
+    product.tags = req.body.tags;
     product.countInStock = req.body.countInStock;
     product.description = req.body.description;
     await product.save();
     await db.disconnect();
-    res.send({ message: 'Product updated successfully' });
+    res.send({ message: "Product updated successfully" });
   } else {
     await db.disconnect();
-    res.status(404).send({ message: 'Product not found' });
+    res.status(404).send({ message: "Product not found" });
   }
 };
 const deleteHandler = async (req, res) => {
@@ -51,10 +51,10 @@ const deleteHandler = async (req, res) => {
   if (product) {
     await product.remove();
     await db.disconnect();
-    res.send({ message: 'Product deleted successfully' });
+    res.send({ message: "Product deleted successfully" });
   } else {
     await db.disconnect();
-    res.status(404).send({ message: 'Product not found' });
+    res.status(404).send({ message: "Product not found" });
   }
 };
 export default handler;
