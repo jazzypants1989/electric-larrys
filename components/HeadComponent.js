2d6def645ee7973f96755633aab7647b615d5ddc
Jesse Pence <jessepence@gmail.com>
2022-10-12 14:00:07 -0700

    that should do it

--- components/HeadComponent.js
+++ components/HeadComponent.js
@@ -8,13 +8,7 @@ const HeadComponent = ({ title, description, tags }) => {
       </title>
       <meta name="description" content={description} />
       <meta name="keywords" content={tags} />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <met charSet="utf-8" />
       <link rel="icon" href="/favicon.ico" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Bangers&display=swap"
-        rel="stylesheet"
-      />
     </Head>
   );
 };
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,10 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
+              <link
+        href="https://fonts.googleapis.com/css2?family=Bangers&display=swap"
+        rel="stylesheet"
+      />
         <Head></Head>
         <body>
           <Main />
