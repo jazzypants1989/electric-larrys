c5f0367089bbfd364e3e64fcf653d329c670b78f
Jesse Pence <jessepence@gmail.com>
2022-11-20 12:41:02 -0800

    ayy

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from "react"
+import { useState, useEffect, useCallback } from "react"
 import { RiArrowLeftSFill, RiArrowRightSFill } from "react-icons/ri"
 import Image from "next/image"
 
