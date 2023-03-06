6fc0c028b3a831663558d6123f9d98d77d0f6d08
Jesse Pence <jessepence@gmail.com>
2023-03-05 20:20:43 -0800

    cue dj khaled

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,17 +1,19 @@
 import { Suspense } from "react"
-import { getProducts } from "../../utils/dataHooks/getProducts"
+import { getProducts, Product } from "../../utils/dataHooks/getProducts"
 import Spinner from "./loading"
 import Products from "./Products"
 
 export default async function ProductsPage() {
-  const products = await getProducts()
+  const products: Product[] = await getProducts()
 
-  const productsInStock = products.filter((product) => product.countInStock > 0)
+  const productsInStock = products.filter(
+    (product: Product) => product.countInStock > 0
+  )
 
-  const categories = productsInStock.map((product) => product.category)
+  const categories = productsInStock.map((product: Product) => product.category)
   const uniqueCategories = [...new Set(categories)]
 
-  const tags = productsInStock.map((product) => product.tags)
+  const tags = productsInStock.map((product: Product) => product.tags)
   const uniqueFlattenedTags = [...new Set(tags.flat())]
 
   return (
